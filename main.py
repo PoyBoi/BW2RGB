0ccb81893c74f451e5f04305c5abb81356fd4232
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-05-02 07:47:37 +0530

    lots of code, idk at this point man

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-from .scripts.colorizeCode import colorize
-from .scripts.denoiser import denoise_image
+import os
+import cv2
+from scripts.colorizeCode import colorize
+from scripts.denoiser import denoise_image
 
-# denoise_image(r"C:\Users\parvs\VSC Codes\Python-root\Bringing-Old-Photos-Back-to-Life\test_images\old_w_scratch\b.png")
-colorize(r"C:\Users\parvs\VSC Codes\Python-root\Bringing-Old-Photos-Back-to-Life\test_images\old_w_scratch\b.png", True, 1, True, True, False)
+print("Init location")
+
+loc = r"C:\Users\parvs\VSC Codes\Python-root\Bringing-Old-Photos-Back-to-Life\test_images\old_w_scratch\b.png"
+loc = r"C:\Users\parvs\Downloads\Test\Done\stage_1_restore_output\masks\input\scratch_removal_automatic_1.png"
+
+img_read = cv2.imread(loc)
+cv2.imshow('image', img_read)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("IMG size = ", img_read.shape)
+
+den_img = denoise_image(loc, denoise_method="nlm", h=3)
+
+print("Denoising Image size = ", den_img[0].shape)
+
+ret_img = colorize(
+    img_obj=den_img[0], 
+    file_org=den_img[1],
+    #r"C:\Users\parvs\VSC Codes\Python-root\Bringing-Old-Photos-Back-to-Life\test_images\old_w_scratch\b.png", 
+    file_loc= None,
+    is_img = True,
+    method = 1,
+    show_graph = True,
+    save_image = True,
+    use_gpu = True
+)
+
+print("Colorized Image size = ", ret_img[1].shape)
+
+for i in range(len(ret_img)):
+    img_rgb = cv2.cvtColor(ret_img[i], cv2.COLOR_BGR2RGB)
+    # cv2.imshow("Image", img_rgb)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
